a2f50c553a295a6e8708af1ff8a357b199c917dc
Monika <moncobra@comcast.net>
2022-11-29 09:50:57 -0800

    first function

---
+++ render-utils.js
@@ -0,0 +1,14 @@
+export function renderPlants(plant) {
+    const plantScienceEl = document.createElement('div');
+    const plantCommonEl = document.createElement('p');
+    const plantGenderEl = document.createElement('p');
+    nameEl.textContent = plant.name;
+    img.src = cat.image_url;
+
+    plantEl.classList.add('allPlants');
+
+    plantEl.append(nameEl, img);
+
+    return catEl;
+}
+//finish this up and edit render function//
